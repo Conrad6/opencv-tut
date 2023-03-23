655c8dbf0b33e6fe85777aaab0d1520adad67b43
Bekondo Conrad <philllittle302@gmail.com>
2023-03-23 09:26:49 +0100

    chapter 1

--- OpenCVCourse/Test.cpp
+++ OpenCVCourse/Test.cpp
@@ -4,11 +4,38 @@
 #include <opencv2/highgui.hpp>
 #include <iostream>
 
+void images();
+void videos();
+void webcam();
 
-int main() {
+void main() {
+	webcam();
+}
+
+void webcam() {
+	cv::VideoCapture capture(0);
+	cv::Mat img;
+	while (true) {
+		capture.read(img);
+		cv::imshow("Webcam", img);
+		cv::waitKey(1);
+	}
+}
+
+void videos() {
+	std::string path = "Resources/video.mp4";
+	cv::VideoCapture capture(path);
+	cv::Mat img;
+	while (true) {
+		capture.read(img);
+		cv::imshow("Videos", img);
+		cv::waitKey(75);
+	}
+}
+
+void images() {
 	std::string path = "Resources/test.jpg";
 	cv::Mat img = cv::imread(path);
-	imshow("OpenCVCourse", img);
+	cv::imshow("Images", img);
 	cv::waitKey(0);
-	return 0;
 }
