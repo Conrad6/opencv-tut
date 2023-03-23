c572cacc96eff4fa35a50ea5d6585b6fb7224899
Bekondo Conrad <philllittle302@gmail.com>
2023-03-23 10:08:15 +0100

    chapter 3: Resize and Crop

--- OpenCVCourse/Test.cpp
+++ OpenCVCourse/Test.cpp
@@ -4,11 +4,19 @@
 #include <opencv2/highgui.hpp>
 #include <iostream>
 
-
+using namespace cv;
+// 375 x 500
 int main() {
 	std::string path = "Resources/test.jpg";
-	cv::Mat img = cv::imread(path);
+	cv::Mat img, imgResized, imgCrop;
+	img = cv::imread(path);
+	resize(img, imgResized, Size(), 1.5, 1.5);
+	Rect roi(0, 0, 150, 250);
+	imgCrop = img(roi);
+	std::cout << img.size;
 	imshow("OpenCVCourse", img);
+	imshow("Img resized", imgResized);
+	imshow("Img crop", imgCrop);
 	cv::waitKey(0);
 	return 0;
 }
